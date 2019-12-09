476c0a1e95beaa981247d6a4d4aed4c644c672d7
jeitzroby <roby.jeitz@hotmail.de>
2019-12-09 11:33:45 +0100

    Old_Project

---
+++ Old_Project/Assets/scripts/shooting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class shooting : MonoBehaviour
+{
+    // Prints a joystick name if movement is detected.
+
+
+    //public SteamVR_Actions_Single squeezeAction;
+
+    /*void Update()
+    {
+        float triggerValue = squeezeAction.GetAxis(SteamVR_Inout_Sources.Any);
+
+        if(triggerValue > 0.0f)
+        {
+            print(triggerValue);
+        }
+    }*/
+}
