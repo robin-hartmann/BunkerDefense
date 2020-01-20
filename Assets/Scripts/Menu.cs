91930853ab747dd881870ce431144c8c52e03f07
Robin Hartmann <contact.robin.hartmann@gmail.com>
2020-01-20 15:52:27 +0100

    Update Menu WIP

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -36,28 +36,49 @@ public class Menu : MonoBehaviour
     public void ShowMenuStart()
     {
         textMenu.GetComponent<Text>().text = "Welcome to Tower Defense!\n\nDestroy the Wheelers with your gun\nbefore they blow up your bunker!";
+
+        Button.ButtonClickedEvent buttonTopOnClick = buttonTop.GetComponent<Button>().onClick;
+        buttonTopOnClick.RemoveAllListeners();
+        buttonTopOnClick.AddListener(HideMenu);
         buttonTop.GetComponentInChildren<Text>().text = "Start";
         buttonTop.SetActive(true);
+
         buttonBottom.SetActive(false);
+
         ShowMenu(Mode.Start);
     }
 
     public void ShowMenuPause(int wheelersDestroyedCount)
     {
         textMenu.GetComponent<Text>().text = $"PAUSED\n\nYou have destroyed {wheelersDestroyedCount} Wheelers until now.";
+
+        Button.ButtonClickedEvent buttonTopOnClick = buttonTop.GetComponent<Button>().onClick;
+        buttonTopOnClick.RemoveAllListeners();
+        buttonTopOnClick.AddListener(HideMenu);
         buttonTop.GetComponentInChildren<Text>().text = "Continue";
         buttonTop.SetActive(true);
-        buttonTop.GetComponentInChildren<Text>().text = "Reset";
+
+        Button.ButtonClickedEvent buttonBottomOnClick = buttonTop.GetComponent<Button>().onClick;
+        buttonBottomOnClick.RemoveAllListeners();
+        buttonBottomOnClick.AddListener(GameManager.Reset);
+        buttonBottom.GetComponentInChildren<Text>().text = "Reset";
         buttonBottom.SetActive(true);
+
         ShowMenu(Mode.Pause);
     }
 
     public void ShowMenuGameOver(int wheelersDestroyedCount)
     {
         textMenu.GetComponent<Text>().text = $"GAME OVER\n\nYou were blown up!\nBut you managed to destroy {wheelersDestroyedCount} Wheelers.";
+
+        Button.ButtonClickedEvent buttonTopOnClick = buttonTop.GetComponent<Button>().onClick;
+        buttonTopOnClick.RemoveAllListeners();
+        buttonTopOnClick.AddListener(GameManager.Reset);
         buttonTop.GetComponentInChildren<Text>().text = "Reset";
         buttonTop.SetActive(true);
+
         buttonBottom.SetActive(false);
+
         ShowMenu(Mode.GameOver);
     }
 
